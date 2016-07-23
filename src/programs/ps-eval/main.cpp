4b70011414a461511614657cdac9cc97851d1bde
Jaak Simm <jaak.simm@gmail.com>
2016-07-23 23:45:31 +0300

    ps-eval working

--- src/programs/ps-eval/main.cpp
+++ src/programs/ps-eval/main.cpp
@@ -2,35 +2,45 @@
 #include <vector>
 #include <pokerstove/penum/ShowdownEnumerator.h>
 
-int main() {
-
-using namespace pokerstove;
 using namespace std;
+using namespace pokerstove;
 
-CardSet completeDeck;
-completeDeck.fill();
-cout << "The whole deck has " << completeDeck.size() << " cards" << endl;
+int main(int argc, char *argv[]) {
+  bool verbose = false;
+  if (argc != 3) {
+    cout << "Need two arguments (got " << (argc - 1) << ")." << endl << " Usage: ps-eval hand1 hand2" << endl;
+    return -1;
+  }
 
-CardDistribution anyTwo;
-anyTwo.fill(completeDeck, 2);
-cout << "There are " << anyTwo.size() << " two card combinations"  << endl;
 
-CardDistribution holeCards;
-holeCards.parse("As6s");
-cout << "There are " << holeCards.size() << " two card combinations"  << endl;
+  CardSet completeDeck;
+  completeDeck.fill();
 
-ShowdownEnumerator showdown;
-vector<EquityResult> result = showdown.calculateEquity(
-    vector<CardDistribution>{anyTwo, holeCards},
+
+  CardDistribution hand1, hand2;
+  hand1.parse(argv[1]);
+  hand2.parse(argv[2]);
+  if (verbose) {
+    cout << "The whole deck has " << completeDeck.size() << " cards" << endl;
+    cout << "Hand 1 '" << argv[1] << "' has " << hand1.size() << " two card combinations"  << endl;
+    cout << "Hand 2 '" << argv[2] << "' has " << hand2.size() << " two card combinations"  << endl;
+  }
+
+  ShowdownEnumerator showdown;
+  vector<EquityResult> result = showdown.calculateEquity(
+    vector<CardDistribution>{hand1, hand2},
     CardSet(""),
     PokerHandEvaluator::alloc("h")
-);
+  );
 
-double shareRandom = result.at(0).winShares + result.at(0).tieShares;
-double shareHand   = result.at(1).winShares + result.at(1).tieShares;
-double total       = shareRandom + shareHand;
+  double share1 = result.at(0).winShares + result.at(0).tieShares;
+  double share2 = result.at(1).winShares + result.at(1).tieShares;
+  double total  = share1 + share2;
 
-cout << "A random hand has "  << shareRandom / total * 100  << " % equity (" << result.at(0).str() << ")" << endl;
-cout << "The hand As6s has "  << shareHand   / total * 100  << " % equity (" << result.at(1).str() << ")" << endl;
-
+  if (verbose) {
+    cout << "Hand 1 has "  << share1 / total * 100  << " % equity (" << result.at(0).str() << ")" << endl;
+    cout << "Hand 2 has "  << share2 / total * 100  << " % equity (" << result.at(1).str() << ")" << endl;
+  } else {
+    cout << share1 / total << endl;
+  }
 }
